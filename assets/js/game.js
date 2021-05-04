689588c407a4c56980181eca55122f0b70bbfd54
Easton Shaum <Eastonshaum@gmail.com>
2021-05-04 10:47:58 -0600

    Add the input for name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
 window.alert("This is an alert! JavaScript is running!");
+
+var playerName = window.prompt("What is your robots name?");
+console.log(playerName)
+var name = "Easton Shaum"
+console.log(name)
+
+function fight() {
+    window.alert("The fight has begun");
+}
+
+//fight();
